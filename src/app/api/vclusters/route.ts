b20917cd2b2f28678ecb89b9e74bd80704775880
Vibhav Bobade <vibhav.bobde@gmail.com>
2024-06-11 16:06:56 +0530

    feat: add service account support so that the dashboard can run in a pod

--- src/app/api/vclusters/route.ts
+++ src/app/api/vclusters/route.ts
@@ -1,18 +1,28 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { KubeConfig, CustomObjectsApi } from '@kubernetes/client-node';
 import { NextResponse } from 'next/server';
-
-const kubeConfig = new KubeConfig();
-kubeConfig.loadFromDefault();
-
-const k8sCustomObjectsApi = kubeConfig.makeApiClient(CustomObjectsApi);
-
-const vClusterGroup = 'vclusters.openvirtualcluster.dev';
-const vClusterVersion = 'v1alpha1';
-const vClusterPlural = 'vclusters';
+import * as fs from 'fs';
 
 export async function GET() {
   try {
+    // Check if running inside a Kubernetes pod
+    const inCluster = fs.existsSync('/var/run/secrets/kubernetes.io/serviceaccount/token');
+
+    const kubeConfig = new KubeConfig();
+    if (inCluster) {
+      // Load the service account credentials from the mounted volumes
+      kubeConfig.loadFromCluster();
+    } else {
+      // Load the default kubeconfig from the user's home directory
+      kubeConfig.loadFromDefault();
+    }
+
+    const k8sCustomObjectsApi = kubeConfig.makeApiClient(CustomObjectsApi);
+
+    const vClusterGroup = 'vclusters.openvirtualcluster.dev';
+    const vClusterVersion = 'v1alpha1';
+    const vClusterPlural = 'vclusters';
+
     // Fetch VClusters from all namespaces
     const response = await k8sCustomObjectsApi.listClusterCustomObject(
       vClusterGroup,
@@ -20,8 +30,7 @@ export async function GET() {
       vClusterPlural
     );
 
-    var clusterList : any = await response.body;
-    // console.log('VClusters:', clusterList);
+    var clusterList: any = await response.body;
     return NextResponse.json(clusterList);
   } catch (error) {
     console.error('Error fetching VClusters:', error);
